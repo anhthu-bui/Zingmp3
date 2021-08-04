7977c7631912a9d74cfbf068998121f4102a59fd
anhthu-bui <anhthu.bui@m.citynow.com>
2021-08-04 22:24:02 +0700

    init source

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['prettier', 'react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'import/no-unresolved': 'off',
+    'react/no-unescaped-entities': 'off',
+    'import/no-extraneous-dependencies': 'off'
+  }
+}
