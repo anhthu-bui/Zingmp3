2279333deee3953eb989ea1a6364ee464f129eb1
anhthu-bui <anhthu.bui@m.citynow.com>
2021-08-12 22:44:08 +0700

    layout footer

--- src/mains/Footer/index.js
+++ src/mains/Footer/index.js
@@ -1,5 +1,84 @@
+// libs
+import { Row, Col } from "antd";
 // others
 import "./style.scss";
+import "antd/dist/antd.css";
+import IconZingmp3 from "../../images/icon_zingmp3.png";
+import IconZalo from "../../images/logo_zalo.png";
+import IconYoutube from "../../images/logo_youtube.png";
+import IconGg from "../../images/logo_gg.png";
+import IconFb from "../../images/logo_fb.jpeg";
+import DmcaProtected from "../../images/dmca_protected.png";
 
-const Footer = () => <div className="footer">Footer</div>;
+const Footer = () => (
+  <div className="footer-wrapper">
+    <Row className="footer-wrapper-inner">
+      <Col span={8} className="copy-right">
+        <img src={IconZingmp3} alt="Logo_Zingmp3" />
+        <p className="info_copy-right">
+          © 2015 <span> VNG Corporation</span>
+        </p>
+        <p>Giấy phép MXH số 314/GP-BTTTT.</p>
+      </Col>
+      <Col span={10}>
+        <ul className="list_footer">
+          <li>
+            <a href="/">Giới thiệu</a>
+          </li>
+          <li>
+            <a href="/">Điều khoản</a>
+          </li>
+          <li>
+            <a href="/">Quảng cáo</a>
+          </li>
+          <li>
+            <a href="/">FAQs</a>
+          </li>
+          <li>
+            <a href="/">Copyright</a>
+          </li>
+          <li>
+            <a href="/">Zing MP3 VIP</a>
+          </li>
+          <li>
+            <a href="/">Ứng dụng</a>
+          </li>
+          <li>
+            <a href="/">Góp ý</a>
+          </li>
+          <li>
+            <a href="/">APIs</a>
+          </li>
+          <li>
+            <a href="/">Liên hệ</a>
+          </li>
+        </ul>
+      </Col>
+      <Col span={6} className="list_logo">
+        <a href="#">
+          <img src={IconFb} alt="Facebook" style={{ width: "35px" }} />
+        </a>
+        <a href="#">
+          <img src={IconZalo} alt="Zalo" style={{ width: "45px" }} />
+        </a>
+        <a href="#">
+          <img src={IconYoutube} alt="Youtube" style={{ width: "35px" }} />
+        </a>
+        <a href="#">
+          <img src={IconGg} alt="Google" style={{ width: "35px" }} />
+        </a>
+        <div className="dmca-badge">
+          <a href="#">
+            <img
+              src={DmcaProtected}
+              alt="Dmca_protected"
+              //   style={{ width: " }}
+            />
+          </a>
+        </div>
+        <a href="#">Phiên bản mobile</a>
+      </Col>
+    </Row>
+  </div>
+);
 export default Footer;
