4625f40daf2d084dfc9da6693f2feb2959d2b365
anhthu-bui <anhthu.bui@m.citynow.com>
2021-08-24 11:27:52 +0700

    handling event for pagination

---
+++ src/hooks/useEventListener.js
@@ -0,0 +1,19 @@
+// libs
+import { useEffect } from "react";
+
+export default function useEventListener(eventName, isHover, handler) {
+  useEffect(
+    () => {
+      const isSupported = window && window.addEventListener;
+      if (!isSupported) return;
+      if (isHover === true) {
+        // Add event listener
+        window.addEventListener(eventName, handler);
+      } else {
+        // Remove event listener
+        window.removeEventListener(eventName, handler);
+      }
+    },
+    [eventName, isHover, handler] // Re-run if eventName or element changes
+  );
+}
